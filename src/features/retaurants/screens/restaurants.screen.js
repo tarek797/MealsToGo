da7432afbc448080d53043d01a5432d93a986120
tarek797 <tarek.soliman797@gmail.com>
2024-05-02 16:05:04 +0200

    List the cards

--- src/features/retaurants/screens/restaurants.screen.js
+++ src/features/retaurants/screens/restaurants.screen.js
@@ -1,7 +1,8 @@
 import { Searchbar } from "react-native-paper";
-import { StatusBar } from "react-native";
+import { FlatList, StatusBar } from "react-native";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import styled from "styled-components/native";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
 const SafeArea = styled.SafeAreaView`
   flex: 1;
@@ -24,9 +25,27 @@ export const RestaurantsScreen = () => {
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
-      <RestaurantListContainer>
-        <RestaurantInfoCard />
-      </RestaurantListContainer>
+      <FlatList
+        data={[
+          { name: 1 },
+          { name: 2 },
+          { name: 3 },
+          { name: 4 },
+          { name: 5 },
+          { name: 6 },
+          { name: 7 },
+          { name: 8 },
+          { name: 9 },
+          { name: 10 },
+        ]}
+        renderItem={() => (
+          <Spacer position="bottom" size="large">
+            <RestaurantInfoCard />
+          </Spacer>
+        )}
+        keyExtractor={(item) => item.name}
+        contentContainerStyle={{ padding: 16 }}
+      />
     </SafeArea>
   );
 };
