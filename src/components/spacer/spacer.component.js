b79be471db2842820ba18fb2da05eac5a5ca2479
tarek797 <tarek.soliman797@gmail.com>
2024-05-02 11:34:31 +0200

    build a spacer component

---
+++ src/components/spacer/spacer.component.js
@@ -0,0 +1,43 @@
+import { View, Text } from "react-native";
+import styled from "styled-components/native";
+
+const TopSmall = styled.View`
+  margin-top: ${(props) => props.theme.space.at(1)};
+`;
+const TopMedium = styled.View`
+  margin-top: ${(props) => props.theme.space.at(2)};
+`;
+
+const TopLarge = styled.View`
+  margin-top: ${(props) => props.theme.space.at(3)};
+`;
+
+const LeftSmall = styled.View`
+  margin-left: ${(props) => props.theme.space.at(1)};
+`;
+const LeftMedium = styled.View`
+  margin-left: ${(props) => props.theme.space.at(2)};
+`;
+
+const LeftLarge = styled.View`
+  margin-left: ${(props) => props.theme.space.at(3)};
+`;
+
+export const Spacer = ({ variant }) => {
+  if (variant === "top.medium") {
+    return <TopMedium />;
+  }
+  if (variant === "top.large") {
+    return <TopLarge />;
+  }
+  if (variant === "left.small") {
+    return <LeftSmall />;
+  }
+  if (variant === "left.medium") {
+    return <LeftMedium />;
+  }
+  if (variant === "left.large") {
+    return <LeftLarge />;
+  }
+  return <TopSmall />;
+};
--- src/features/retaurants/components/restaurant-info-card.component.js
+++ src/features/retaurants/components/restaurant-info-card.component.js
@@ -1,7 +1,10 @@
-import { Card, Text } from "react-native-paper";
+import { Card } from "react-native-paper";
+import { Image, Text, View } from "react-native";
 import styled from "styled-components/native";
 import star from "../../../../assets/star";
+import open from "../../../../assets/open";
 import { SvgXml } from "react-native-svg";
+import { Spacer } from "../../../components/spacer/spacer.component";
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
 `;
@@ -18,25 +21,57 @@ const Address = styled(Text)`
   font-family: ${(props) => props.theme.fonts.body};
   font-size: ${(props) => props.theme.fontSizes.caption};
 `;
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space.at(2)};
+  padding-bottom: ${(props) => props.theme.space.at(2)};
+`;
 
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
+`;
 export const RestaurantInfoCard = ({ retaurant = {} }) => {
   const {
     name = "some restaurant",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     photos = [
       "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
     ],
     address = ["100 some random street"],
     isOpenNow = true,
     rating = 4,
-    isClosedTemporari,
+    isClosedTemporarily = true,
   } = retaurant;
+  const ratingArray = Array.from(new Array(Math.ceil(rating)));
   return (
     <RestaurantCard elevation={5}>
       <RestaurantCardCover key={name} source={{ uri: photos.at(0) }} />
       <Card.Content>
         <Title>{name}</Title>
-        <SvgXml xml={star} />
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+            <SectionEnd>
+              {isClosedTemporarily && (
+                <Text variant="label" style={{ color: "red" }}>
+                  CLOSED TEMPORARILY
+                </Text>
+              )}
+              <Spacer variant={"left.large"} />
+              {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+              <Spacer variant={"left.large"} />
+              <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+            </SectionEnd>
+          </Rating>
+        </Section>
         <Address>{address}</Address>
       </Card.Content>
     </RestaurantCard>
