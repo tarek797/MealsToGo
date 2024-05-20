7c48738398e6b4916d4687b46e11119d8ffb1db3
Tarek Habash <tarek.soliman797@gmail.com>
2024-05-20 22:38:18 +0200

    search for restaurants

--- src/features/retaurants/screens/restaurants.screen.js
+++ src/features/retaurants/screens/restaurants.screen.js
@@ -1,44 +1,42 @@
-import { ActivityIndicator, Searchbar } from "react-native-paper";
-import { FlatList, View } from "react-native";
-import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import React, { useContext } from "react";
+import { FlatList } from "react-native";
 import styled from "styled-components/native";
-import { Spacer } from "../../../components/spacer/spacer.component";
+import { ActivityIndicator } from "react-native-paper";
+
 import { SafeArea } from "../../../components/utility/safe-area.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
+
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
-import { useContext } from "react";
-import { theme } from "../../../infrastructure/theme";
+
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import Search from "../components/search.component";
 
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
 
-const RestaurantListContainer = styled.View`
-  flex: 1;
-  padding: ${(props) => props.theme.space.at(3)};
-  background-color: lightblue;
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
 `;
-
 const LoadingContainer = styled.View`
-  position: "absolute";
+  position: absolute;
   top: 50%;
+  left: 50%;
 `;
 
 export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
-  if (isLoading)
-    return (
-      <SafeArea>
-        <LoadingContainer>
-          <ActivityIndicator
-            size={50}
-            animating={true}
-            color={theme.colors.brand.secondary}
-          />
-        </LoadingContainer>
-      </SafeArea>
-    );
   return (
     <>
-      <Search/>
-      <FlatList
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} color="blue" />
+        </LoadingContainer>
+      )}
+      <Search />
+      <RestaurantList
         data={restaurants}
         renderItem={({ item }) => {
           return (
@@ -48,7 +46,6 @@ export const RestaurantsScreen = () => {
           );
         }}
         keyExtractor={(item) => item.name}
-        contentContainerStyle={{ padding: 16 }}
       />
     </>
   );
--- src/services/location/location.context.js
+++ src/services/location/location.context.js
@@ -1,41 +1,41 @@
-import { createContext, useEffect, useState } from "react";
+import React, { useState, useEffect, createContext } from "react";
 import { locationRequest, locationTransform } from "./location.service";
 
 export const LocationContext = createContext();
 export const LocationContextProvider = ({ children }) => {
-  const [keyword, setKeyword] = useState("antwerp");
+  const [keyword, setKeyword] = useState("San Francisco");
   const [location, setLocation] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  function onSearch(searchKeyword) {
-    console.log(location)
+  const onSearch = (searchKeyword) => {
     setIsLoading(true);
     setKeyword(searchKeyword);
-    if(!searchKeyword.length){
-      return 
+    if (!searchKeyword.length) {
+      return;
     }
     locationRequest(searchKeyword.toLowerCase())
       .then(locationTransform)
       .then((result) => {
         setIsLoading(false);
         setLocation(result);
+        console.log(result);
       })
       .catch((err) => {
         setIsLoading(false);
         setError(err);
-        console.log(error)
-
       });
-  }
-
-  useEffect(() => {
-    onSearch(keyword);
-  }, [keyword]);
+  };
 
   return (
     <LocationContext.Provider
-      value={{ location, isLoading, error, search: onSearch, keyword }}
+      value={{
+        isLoading,
+        error,
+        location,
+        search: onSearch,
+        keyword,
+      }}
     >
       {children}
     </LocationContext.Provider>
