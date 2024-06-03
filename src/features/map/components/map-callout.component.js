419bf0c97fe64623bd89b3ab9d6000dd64df8b0a
Tarek Habash <tarek.soliman797@gmail.com>
2024-06-04 01:41:58 +0200

    fix bug on android (compact image)

--- src/features/map/components/map-callout.component.js
+++ src/features/map/components/map-callout.component.js
@@ -1,4 +1,4 @@
 import { CompactRestaurantInfo } from "../../../components/restaurant/compact-restaurant-info.component";
 export const MapCallout = ({ restaurant }) => {
-  return <CompactRestaurantInfo restaurant={restaurant} />;
+  return <CompactRestaurantInfo isMap={true} restaurant={restaurant} />;
 };
